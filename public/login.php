6039b4427a8daccfae12fbea07707b59621130a8
TuggyBoat <39076583+TuggyBoat@users.noreply.github.com>
2025-04-09 18:02:40 -0500

    fix login extra character

--- public/login.php
+++ public/login.php
@@ -65,7 +65,7 @@ if (isset($_POST['submit'])) {
     <button type="button" onclick="window.location.href='index.html'" class="back-btn">Back</button>
 
 </div>
-<<!-- Load Bootstrap bundle first -->
+<!-- Load Bootstrap bundle first -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
 
 <script>
